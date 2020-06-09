a0d8875925eb327e5674ea8aa4fbc39f7abbb58d
Ethan Waxman <ejw99@cornell.edu>
2020-06-09 18:01:43 -0400

    Continued to work on game and main files

--- game.rb
+++ game.rb
@@ -1,8 +1,21 @@
+require_relative 'board'
+
 class Game
     def initialize(p1, p2)
         @player1 = p1.get_name
-        puts "#{@player1}"
         @player2 = p2.get_name
-        puts "#{@player2}"
+        @game_board = Board.new()
+        @x_win = false
+        @o_win = false
+    end
+
+    def win?
+        if @game_board.row_win(:x)
+            @x_win = true
+            return true
+        elsif @game_board.row_win(:o)
+            @o_win = true
+            return true
+        end
     end
 end
--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
 require_relative ('game')
 require_relative ('player')
 
-def create_player(player_number)
+def create_player(player_number, symbol)
     puts "Enter Player #{player_number}'s name:"
     name = gets.chomp
-    player = Player.new(name, :x)
+    player = Player.new(name, symbol)
 end
 
-game = Game.new(create_player(1), create_player(2))
+game = Game.new(create_player(1, :x), create_player(2, :o))
