da514b622ae5c6f0da3992c0dea7ed1348932553
Ethan Waxman <ejw99@cornell.edu>
2020-06-10 16:22:42 -0400

    Added protection against user input, as well as added a way to quit the game early

--- game.rb
+++ game.rb
@@ -39,28 +39,25 @@ class Game
             @game_board.print_board
             if @p1_turn
                 puts "It is #{@player1}'s turn"
-                puts "Please enter where you would like to mark:"
-                input = gets.chomp
-                while @game_board.get_tile(input) != " "
-                    puts "Please enter an unmarked tile:"
-                    input = gets.chomp
-                end
-                @game_board.mark_tile(:x, input)
-                victory() if win?()
+                take_turn(:x)
                 @p1_turn = false
             else
                 puts "It is #{@player2}'s turn"
-                puts "Please enter where you would like to mark:"
-                input = gets.chomp
-                while @game_board.get_tile(input) != " "
-                    puts "Please enter an unmarked tile:"
-                    input = gets.chomp
-                end
-                @game_board.mark_tile(:o, input)
-                victory() if win?()
+                take_turn(:o)
                 @p1_turn = true
             end
         end
+    end
+
+    def take_turn(sym)
+        puts "Please enter where you would like to mark or enter 'quit' to exit the game:"
+        input = gets.chomp
+        until valid_input?(input)
+            puts "Please enter a valid tile:"
+            input = gets.chomp
+        end
+        @game_board.mark_tile(sym, input)
+        victory() if win?()
     end
 
     def victory
@@ -80,7 +77,6 @@ class Game
         end
     end
 
-
     def play_again
         puts ""
         puts "Would you like to play again? (y/n)"
@@ -97,5 +93,19 @@ class Game
         else
             puts "Bruh..."
         end
+    end
+
+    def valid_input?(input)
+        end_game_early() if input == "quit"
+        return false unless input.length == 2
+        return false unless input[0] == "a" || input[0] == "b" || input[0] == "c"
+        return false unless input[1] == "1" || input[1] == "2" || input[1] == "3"
+        return false unless @game_board.get_tile(input) == " "
+        return true
+    end
+
+    def end_game_early
+        puts "Thanks for playing!"
+        exit
     end
 end
